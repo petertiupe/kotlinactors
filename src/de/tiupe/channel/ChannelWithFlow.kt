4f663eb43123db1533b4d5a89f2f3315833a3183
peter <peter@tiupe.de>
2020-06-09 11:55:37 +0200

    Flow -> Channel --> Flow - Example

--- src/de/tiupe/channel/ChannelWithFlow.kt
+++ src/de/tiupe/channel/ChannelWithFlow.kt
@@ -1,12 +1,9 @@
 package de.tiupe.channel
 
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
-import kotlinx.coroutines.channels.consume
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -15,6 +12,7 @@ fun intFlowFunction(): Flow<Int> = flow {
         delay(1000) // pretend we are asynchronously waiting 100 ms
         emit(i) // emit next value
     }
+
 }
 
 class ChannelReceiver(val channel: ConflatedBroadcastChannel<Int>)
@@ -25,22 +23,37 @@ fun main() = runBlocking {
     // Ein channel sollte mehrere Receiver besitzen können.
     val receiver1 = ChannelReceiver(channel)
     val receiver2 = ChannelReceiver(channel)
+    val receiver3 = ChannelReceiver(channel)
 
     // Der Channel bekommt die Werte des Flows von Int herein und stellt sie einem etwaigen Receiver zur
     // Verfügung.
-    val job1 = launch {
-        intFlowFunction().collect { value ->
+    val offerJob = launch {
+        // Der Channel stellt immer nur den aktuellsten Wert zur Verfügung.
+        intFlowFunction().collectLatest { value ->
             delay(300) // pretend we are processing it for 300 ms
             channel.offer(value)
         }
+        // Der Code in der Coroutine läuft synchron. Solange noch Werte aus dem Flow
+        // kommen, sendet der Channel. Die unten per launch-gestarteten Jobs stoppen
+        // nur, wenn der channel ein close sendet.
+        channel.close()
     }
 
-    val job2 = launch {
-        receiver1.channel.consumeEach { zahl -> println("Receiver 1 meldet die Zahl: $zahl") }
-    }
-    val job3 = launch {
-        receiver2.channel.consume { println("Receiver 2 meldet die Zahl: $this") }
+    val receiver1Job = launch {
+        receiver1.channel.consumeEach { println("Receiver 1 meldet die Zahl: $it") }
     }
 
-    println("Jetzt fängt die Arbeit erst an ...")
+    val receiver2Job = launch {
+        receiver2.channel.consumeEach { println("Receiver 2 meldet die Zahl: $it") }
+    }
+
+    // So kann man auf der Empfängerseite wieder einen Flow zur Verfügung stellen.
+    // Der Flow kann dann wieder wie gewohnt verarbeitet werden, hier mit einem collect,
+    // damit er auch terminiert.
+    val receiverToFlowJob = launch {
+        val flowOfIntFromChannel: Flow<Int> = receiver3.channel.asFlow()
+        flowOfIntFromChannel.collect {println("Aus demFlow von Receiver 3 kommt: $it")}
+    }
+
+    println("Jetzt fängt die Arbeit erst an ... allerdings weiter oben :-) !")
 }
