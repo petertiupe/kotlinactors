6d2d504187ac418d4bc914427a297931d0350742
peter <peter@tiupe.de>
2020-07-22 17:41:38 +0200

    Java-Observable-Example with data-holder

--- src/de/tiupe/observerpattern/observerAndObservable.kt
+++ src/de/tiupe/observerpattern/observerAndObservable.kt
@@ -4,6 +4,11 @@ import java.util.*
 
 // Observable ist die Klasse, die die Daten hält, die also observable (beobachtbar) sein soll.
 // Die Observer werden dann informiert, wenn ein Wert sich geändert hat.
+
+// Der folgende Artikel ist eine gute Einführung in die reaktive Progammierung:
+
+//  https://blog.danlew.net/2017/07/27/an-introduction-to-functional-reactive-programming/
+
 class ObservableData(intValue: Int) : Observable() {
     // So lange die Observable nicht informiert, passiert bei den Observern gar nichts...
     var intValue: Int = intValue
@@ -17,7 +22,7 @@ class ObservableData(intValue: Int) : Observable() {
 }
 
 fun main() {
-
+    // marble diagramms show how to use this...
     val observer: Observer = object : Observer{
         override fun update(o: Observable?, arg: Any?) {
             when(o) {
@@ -28,17 +33,17 @@ fun main() {
         }
     }
 
-    val observDat = ObservableData(intValue = 1)
+    val observableData = ObservableData(intValue = 1)
     // So lange die Observable nicht informiert, passiert bei den Observern gar nichts...
-    observDat.addObserver(observer)
+    observableData.addObserver(observer)
 
     // Ab hier funktioniert der Mechanismus automatisch. Sobald der Int-Wert sich ändert, bekommt der Observer
     // das mit und kann auf den neuen Wert reagieren. Wichtig ist, dass der Observer bestimmt, ob er was macht oder
     // nicht, nicht der Observable Aus meiner Sicht gibt es an dieser Stelle allerdings noch die Lücke, dass der
     // Observable all seine Observer kennen muss. Dadurch ist eine enge Kopplung zwischen Observable und Observer
     // gegeben.
-    observDat.intValue = 4
-    observDat.intValue = 6
+    observableData.intValue = 4
+    observableData.intValue = 6
 
 }
 
