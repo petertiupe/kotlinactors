a5244ba63e14f183079143587e96887ef42b4a6f
peter <peter@tiupe.de>
2020-12-30 22:01:17 +0100

    CrossinlineExample.kt added

---
+++ src/de/tiupe/crossinline/crossinlineExample.kt
@@ -0,0 +1,26 @@
+package de.tiupe.crossinline
+
+/*
+* Die Crossinline-Anweisung sorgt dafür, dass die Programmsteuerung nicht von einem übergebenen
+* Lambda übernommen werden kann und zwar nur bei einer inline-Funktion.
+* Mit der inline-Anweisung würde die return - Anweisung aus der multiplyByTwo
+* in die main-Funktion gelangen und dort u. U. den Programmfluss steuern. Um
+* dies zu unterbinden, kann man innerhalb der Inline-Funktion das Lambda mit crossinline
+* markieren. Dadurch wird das übergebene return in der MultiplyByTwo zu einem Fehler.
+* Hier habe ich es nur "auskommentiert" stehen, weil sonst der Code nicht mehr kompiliert.
+* */
+
+fun main() {
+    multiplyByTwo(5) {
+        println("Das Ergebnis ist: $it")
+        return
+    }
+}
+
+inline fun multiplyByTwo(num: Int, /*crossinline*/ lambda:  (Int) -> Unit) : Int {
+    val result = num * 2
+    lambda.invoke(result)
+    return result
+}
+
+
