2e4a9106d2459e20c27f60be9d4f531c15640e86
peter <peter@tiupe.de>
2021-01-03 22:32:38 +0100

    numeric integration now parallel

--- src/main/kotlin/de/tiupe/asynchnachbuch/effektiveintegration.kt
+++ src/main/kotlin/de/tiupe/asynchnachbuch/effektiveintegration.kt
@@ -1,5 +1,9 @@
 package de.tiupe.asynchnachbuch
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 // Anzahl der Integrations-Schritte
 const val MAXSTEPS = 10_000_000
 
@@ -19,6 +23,21 @@ fun integrateNumeric(start: Double, end:Double, func: (Double) -> Double): Doubl
     return result * delta
 }
 
+
+// Im Buch machen sie dies noch mit einer mutable List und einem await all auf der Liste,
+// das Prinzip ist jedoch dasselbe.
+suspend fun integrateParallel(start: Double, end:Double , func: (Double) -> Double): Double {
+    val x = integrateNumeric(start, end, func)
+    val intervallLaenge = (end - start) / 2
+    val def1 = GlobalScope.async {
+        integrateNumeric(start, start + intervallLaenge, func)
+    }
+    val def2 = GlobalScope.async {
+        integrateNumeric(start + intervallLaenge, start + 2 * intervallLaenge, func)
+    }
+    return def1.await() + def2.await()
+}
+
 fun main(){
     // einfache Funktion f(x) = x^2
     // Integral = 1/3 x^3
@@ -26,6 +45,16 @@ fun main(){
     // 1/3 * 27 - 1/3 * 1 = 8 2/3
     // geliefert wird beim ersten Lauf 8.666667666667417
 
-    val result = integrateNumeric(1.0, 3.0, {d  -> d * d})
+    val result = integrateNumeric(1.0, 3.0, { d  -> d * d })
     println("Das Ergebnis ist: $result")
+
+    GlobalScope.launch {
+        val parallelResult = integrateParallel(1.0, 3.0, { d -> d * d })
+        println("Parallel gerechnet ist das Ergebnis: $parallelResult")
+    }
+
+    // Es muss wieder gewartet werden
+    Thread.sleep(10_000)
+
+
 }
